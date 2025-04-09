6173beabbd6469aa0e60cc1cc8b3134a2ee9fd94
Lucas Nicolau <nicolau.lucas04@gmail.com>
2025-04-09 18:01:35 -0400

    Add files via upload

---
+++ Operações 1 e 2/segundos.c
@@ -0,0 +1,1 @@
+#include <stdio.h> int main(void){ int horas , minutos , segundos , soma; scanf("%d: %d: %d" , &horas , &minutos , &segundos); soma = (horas * 3600) +(minutos * 60) + segundos; printf("%d \n" , soma); return 0; }
---
+++ Operações 1 e 2/soma.c
@@ -0,0 +1,1 @@
+#include <stdio.h> int main(void){ int n1 , n2 , n3 , soma; scanf("%d" , &n1); scanf("%d" , &n2); scanf("%d" , &n3); soma = n1 + n2 + n3; printf("%d \n" , soma); return 0; }

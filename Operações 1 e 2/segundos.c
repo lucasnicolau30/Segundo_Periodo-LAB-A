9a7a7301cb7fab75b55aa6faf5872dd6435dfb3a
Lucas Nicolau <nicolau.lucas04@gmail.com>
2025-04-15 10:53:25 -0400

    Atualizar o segundos.c

--- Operações 1 e 2/segundos.c
+++ Operações 1 e 2/segundos.c
@@ -1,1 +1,10 @@
-#include <stdio.h> int main(void){ int horas , minutos , segundos , soma; scanf("%d: %d: %d" , &horas , &minutos , &segundos); soma = (horas * 3600) +(minutos * 60) + segundos; printf("%d \n" , soma); return 0; }
+#include <stdio.h> 
+
+int main(void){ 
+    int horas , minutos , segundos , soma; 
+    scanf("%d: %d: %d" , &horas , &minutos , &segundos); 
+    soma = (horas * 3600) +(minutos * 60) + segundos; 
+    printf("%d \n" , soma); 
+    
+    return 0; 
+}

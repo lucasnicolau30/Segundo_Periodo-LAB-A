7ad5fa43f4b987d5ff534b27b255292a7885f132
Lucas Nicolau <nicolau.lucas04@gmail.com>
2025-04-15 10:45:35 -0400

    Atualizar o difHora.c

--- Operações 1 e 2/difHora.c
+++ Operações 1 e 2/difHora.c
@@ -1,1 +1,15 @@
-#include <stdio.h> int main(void){ int horas1 , minutos1 , segundos1 , soma1; int horas2 , minutos2 , segundos2 , soma2; int diferenca; scanf("%d: %d:%d" , &horas1 , &minutos1 , &segundos1); soma1 = (horas1 * 3600) + (minutos1 * 60) + segundos1; scanf("%d: %d: %d" , &horas2 , &minutos2 , &segundos2);soma2 = (horas2 * 3600) + (minutos2 * 60) + segundos2; diferenca = soma1 - soma2; printf("%d \n" , diferenca); return 0; }
+#include <stdio.h> 
+
+int main(void){ 
+    int horas1 , minutos1 , segundos1 , soma1; 
+    int horas2 , minutos2 , segundos2 , soma2; 
+    int diferenca; 
+    scanf("%d: %d:%d" , &horas1 , &minutos1 , &segundos1); 
+    soma1 = (horas1 * 3600) + (minutos1 * 60) + segundos1; 
+    scanf("%d: %d: %d" , &horas2 , &minutos2 , &segundos2);
+    soma2 = (horas2 * 3600) + (minutos2 * 60) + segundos2; 
+    diferenca = soma1 - soma2; 
+    printf("%d \n" , diferenca); 
+    
+    return 0; 
+}

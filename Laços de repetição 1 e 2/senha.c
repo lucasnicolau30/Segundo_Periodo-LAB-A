d8a3c07f3845010ae78f701a91379a67bea4d796
Lucas Nicolau <nicolau.lucas04@gmail.com>
2025-04-15 11:48:13 -0400

    Atualizar o senha.c

--- Laços de repetição 1 e 2/senha.c
+++ Laços de repetição 1 e 2/senha.c
@@ -1,1 +1,16 @@
-#include <stdio.h> int main(){ int vezes , num; while(scanf("%d" , &vezes)){ scanf("%d" , &num); if(vezes == 0 && num == 0){ break; } for(int i = 0 ; i < vezes ; i ++){ printf("%d" , num); } } return 0; }
+#include <stdio.h> 
+
+int main(){ 
+    int vezes , num; 
+    while(scanf("%d" , &vezes)){ 
+        scanf("%d" , &num); 
+        if(vezes == 0 && num == 0){ 
+            break; 
+        } 
+        for(int i = 0 ; i < vezes ; i ++){ 
+            printf("%d" , num); 
+        } 
+    } 
+    
+    return 0; 
+}

b9e1fd85a75e248db5cf4a29227ee0b483a63238
Lucas Nicolau <nicolau.lucas04@gmail.com>
2025-04-15 10:59:15 -0400

    Atualizar o codibentinho.c

--- Laços de repetição 1 e 2/codibentinho.c
+++ Laços de repetição 1 e 2/codibentinho.c
@@ -1,1 +1,26 @@
-#include <stdio.h> int main(){ int vezes; scanf("%d" , &vezes); for(int i = 0 ; i < vezes ; i ++){ int dia , temp; scanf("%d %d" , &dia , &temp); if((dia == 3 || dia == 5) && temp >=15){ printf("jeans \n"); } else if((dia == 2 && temp > 23) || temp >= 25){ printf("bermuda \n"); } else if(temp < 15){printf("F \n"); } else{ printf("jeans \n"); } } return 0; }
+#include <stdio.h> 
+
+int main(){ 
+    int vezes; 
+    scanf("%d" , &vezes); 
+    
+    for(int i = 0 ; i < vezes ; i ++){ 
+        int dia , temp; 
+        scanf("%d %d" , &dia , &temp); 
+        
+        if((dia == 3 || dia == 5) && temp >=15){ 
+            printf("jeans \n"); 
+        } 
+        else if((dia == 2 && temp > 23) || temp >= 25){ 
+            printf("bermuda \n"); 
+        } 
+        else if(temp < 15){
+            printf("F \n");
+        } 
+        else{ printf("jeans \n"); 
+            
+        } 
+    } 
+    
+    return 0; 
+}

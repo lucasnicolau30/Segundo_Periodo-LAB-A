511af59811eab26c695e5e2a7d9a2c59515ab93b
Lucas Nicolau <nicolau.lucas04@gmail.com>
2025-04-15 11:46:24 -0400

    Atualizar o pares.c

--- Laços de repetição 1 e 2/pares.c
+++ Laços de repetição 1 e 2/pares.c
@@ -1,1 +1,21 @@
-#include <stdio.h> int main(){ int n , m; while(scanf("%d %d" , &n , &m)){ int soma = 0 , cont = n; while(cont <= m){ if(cont % 2 == 0){ soma += cont;} cont ++; } if(n == 0 && m == 0){ break; } printf("%d \n" , soma); } return 0; }
+#include <stdio.h> 
+
+int main(){ 
+    int n , m; 
+    
+    while(scanf("%d %d" , &n , &m)){ 
+        int soma = 0 , cont = n; 
+        while(cont <= m){ 
+            if(cont % 2 == 0){ 
+                soma += cont;
+            } 
+            cont ++; 
+        } 
+        if(n == 0 && m == 0){ 
+            break; 
+        } 
+        printf("%d \n" , soma); 
+    } 
+    
+    return 0; 
+}

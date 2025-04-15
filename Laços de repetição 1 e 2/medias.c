81c893c3743e569b63d2e21d53158a176f3e5555
Lucas Nicolau <nicolau.lucas04@gmail.com>
2025-04-15 11:00:42 -0400

    Atualizar o medias.c

--- Laços de repetição 1 e 2/medias.c
+++ Laços de repetição 1 e 2/medias.c
@@ -1,1 +1,17 @@
-#include <stdio.h> int main(void){ int qntd; float num , media; while(scanf("%d" , &qntd) , qntd !=0){ float soma = 0.0; for(int i = 0 ; i < qntd ; i++){ scanf("%f" , &num); soma += num; } media = soma / qntd; printf("%.2f \n" , media); } return 0; }
+#include <stdio.h> 
+
+int main(void){ 
+    int qntd; 
+    float num , media; 
+    
+    while(scanf("%d" , &qntd) , qntd !=0){ 
+        float soma = 0.0; 
+        for(int i = 0 ; i < qntd ; i++){ 
+            scanf("%f" , &num); 
+            soma += num; 
+        } 
+        media = soma / qntd; 
+        printf("%.2f \n" , media); 
+    } 
+    return 0; 
+}
